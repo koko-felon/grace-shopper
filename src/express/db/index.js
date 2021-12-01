4c6d2ce15443a52edb6734dc17beb4727b826439
Tri <16trile@gmail.com>
2021-12-01 10:30:00 -0600

    herofu config

--- src/express/db/index.js
+++ src/express/db/index.js
@@ -5,6 +5,10 @@ const DB_URL =
   process.env.DATABASE_URL || `postgres://localhost:5432/${DB_NAME}`;
 const client = new Client({
   connectionString: DB_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : undefined,
 });
 
 // database methods
