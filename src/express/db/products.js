20e8722afc3c7381a23da4e7d59091c06a496330
Tri <16trile@gmail.com>
2021-12-02 13:56:33 -0600

    finished v1 of products methods

---
+++ src/express/db/products.js
@@ -0,0 +1,106 @@
+const client = require("./client");
+
+async function createProduct({
+  categoryId,
+  SKU,
+  productName,
+  productDescription,
+  currentPrice,
+  productQuantity,
+  discount,
+  MSRP,
+  image,
+}) {
+  const {
+    rows: [product],
+  } = await client.query(
+    `
+      INSERT INTO products("categoryId", "SKU", "productName", "productDescription" , "currentPrice" , "productQuantity", discount, "MSRP", image)
+      VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9)
+      RETURNING *;
+    `,
+    [
+      categoryId,
+      SKU,
+      productName,
+      productDescription,
+      currentPrice,
+      productQuantity,
+      discount,
+      MSRP,
+      image,
+    ]
+  );
+  return product;
+}
+
+async function getProducts() {
+  const {
+    rows: [product],
+  } = await client.query(
+    `
+        SELECT * FROM products;
+    `
+  );
+
+  return [product];
+}
+
+async function getProductById(productId) {
+  const {
+    rows: [product],
+  } = await client.query(
+    `
+         SELECT * FROM products WHERE id=$1;
+        `,
+    [productId]
+  );
+
+  return product;
+}
+
+async function updateProduct(id, ...fields) {
+  const fieldNames = Object.keys(fields);
+
+  const setString = fieldNames
+    .map((fieldName, index) => {
+      return `${fieldName}=$${index + 2}`;
+    })
+    .join(",");
+
+  const fieldValues = Object.values(fields);
+
+  const { rows } = await client.query(
+    `
+  UPDATE products SET ${setString}
+  WHERE id = $1
+  RETURNING *
+  `,
+    [id, ...fieldValues]
+  );
+
+  const [product] = rows;
+
+  return product;
+}
+
+async function deleteProduct(productId) {
+  const {
+    rows: [product],
+  } = await client.query(
+    `
+        DELETE FROM products WHERE id=$1 
+        `,
+    [productId]
+  );
+
+  return product;
+}
+
+module.exports(
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct
+);
