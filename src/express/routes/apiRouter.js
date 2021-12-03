dda0b0ec953a02bcd07122a53bca9a0a4f49a176
Pawan Benjamin <pawanbenjamin@gmail.com>
2021-12-03 13:37:09 -0500

    prodcuts router example

--- src/express/routes/apiRouter.js
+++ src/express/routes/apiRouter.js
@@ -1,13 +1,15 @@
-const apiRouter = require('express').Router();
+const apiRouter = require('express').Router()
 
 // GET /api
 // This is a sample route
-apiRouter.get("/", (req, res, next) => {
+apiRouter.get('/', (req, res, next) => {
   res.send({
-    message: "API is under construction!"
-  });
-});
+    message: 'API is under construction!',
+  })
+})
+
+apiRouter.use('/products', require('./products'))
 
 // use your sub-routers here
 
-module.exports = apiRouter;
+module.exports = apiRouter
---
+++ src/express/routes/products.js
@@ -0,0 +1,8 @@
+const productsRouter = require('express').Router()
+
+productsRouter.get('/', async (req, res, next) => {
+  try {
+  } catch (error) {}
+})
+
+module.exports = productsRouter
