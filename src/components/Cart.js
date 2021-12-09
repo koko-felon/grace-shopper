6e20070f961b9b92a7088e4350b8081303928229
Rizzo4 <a.ruvalcaba4@gmail.com>
2021-12-09 15:01:28 -0600

    some cart tings

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function Cart(props) {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    const getCart = async () => {
+      const response = await fetch(`/users/:userId/cart`);
+      const data = await response.json();
+      setCart(data);
+    };
+
+    getCart();
+  }, []);
+
+  const cart
+
   return (
     <>
       <div>
