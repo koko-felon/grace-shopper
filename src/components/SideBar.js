dade5feab8040cffc6fbf6fcab822a23282e1f8e
Tri <16trile@gmail.com>
2021-12-09 14:09:45 -0600

    added sidebar/weekly steals

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
+import Nav from "./Nav";
+import Footer from "./Footer";
 
 function Sidebar() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch(`/api/products`);
+      const data = await response.json();
+      setProducts(data);
+    };
+
+    getProducts();
+  }, []);
+
+  const productsToFilter = products.filter(
+    (product) => product.id === 6 || product.id === 7 || product.id === 9
+  );
+
+  const productsToRender = productsToFilter.map((product) => {
+    return (
+      <>
+        <div>
+          <h2>Product: {product.productName}</h2>
+          <img src={product.image} />
+          <p>Price: {product.currentPrice}</p>
+          <p>Quantity: {product.productQuantity}</p>
+        </div>
+      </>
+    );
+  });
+
   return (
-    <div>
-      This is the sidebar!
-      <Link to="/ThisWeeksSteals">Link to ThisWeeksSteals!</Link>
-    </div>
+    <>
+      <Nav />
+      <h1>This is the sidebar!!!!!</h1>
+      <h2>
+        <Link to="/ThisWeeksSteals">This Weeks Steals!</Link>
+      </h2>
+      {productsToRender}
+      <Footer />
+    </>
   );
 }
 
--- src/components/ThisWeeksSteals.js
+++ src/components/ThisWeeksSteals.js
@@ -1,12 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Nav from "./Nav";
 import Footer from "./Footer";
 
 function ThisWeeksSteals(props) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      const response = await fetch(`/api/products`);
+      const data = await response.json();
+      setProducts(data);
+    };
+
+    getProducts();
+  }, []);
+
+  const productsToFilter = products.filter(
+    (product) => product.id === 6 || product.id === 7 || product.id === 9
+  );
+
+  const productsToRender = productsToFilter.map((product) => {
+    return (
+      <>
+        <div>
+          <h2>Product: {product.productName}</h2>
+          <img src={product.image} />
+          <p>Description: {product.productDescription}</p>
+          <p>Price: {product.currentPrice}</p>
+          <p>Quantity: {product.productQuantity}</p>
+          <p>MSRP: {product.MSRP}</p>
+          <p>SKU: {product.SKU}</p>
+        </div>
+      </>
+    );
+  });
+
   return (
     <>
       <Nav />
-      <div>This is the Weekly Steals Page!</div>
+      <h1>THIS WEEKS STEALS!</h1>
+      {productsToRender}
       <Footer />
     </>
   );
