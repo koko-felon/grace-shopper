24e9dea1e0c94116a9250b58acae25f88e1fff73
Rizzo4 <a.ruvalcaba4@gmail.com>
2021-12-13 05:11:11 -0600

    ...

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -12,10 +12,50 @@ function Cart(props) {
       const response = await fetch(`/api/orders/users/${userState.id}/cart`);
       const data = await response.json();
       cartDispatch({ type: "SET_CART", value: data });
+      console.log(data);
     };
 
     getCart();
   }, []);
+
+  const products =
+    cartState.products &&
+    cartState.products.length &&
+    cartState.products.map((product) => {
+      return (
+        <div key={product.productId}>
+          <img src={product.imageUrl} />
+          <h3>{product.productName}</h3>
+          <h4>${product.currentPrice / 100}</h4>
+          <h5>{product.productQuantity}</h5>
+          <button
+            onClick={() => removeFromCart(product.productId, cartState.orderId)}
+          >
+            Remove from Cart
+          </button>
+          <span>QTY</span>
+          <button
+            onClick={() => increaseQty(product.productId, product.qty + 1)}
+          >
+            +
+          </button>
+          <button
+            onClick={() => decreaseQty(product.productId, product.qty - 1)}
+          >
+            -
+          </button>
+        </div>
+      );
+    });
+
+  const total =
+    cartState.products &&
+    cartState.products.length &&
+    cartState.products
+      .map((product) => {
+        return product.currentPrice * product.quantity;
+      })
+      .reduce((prevTotal, currTotal) => prevTotal + currTotal);
 
   return (
     <>
